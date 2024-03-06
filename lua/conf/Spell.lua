40dc4027526e81a03c4d20e518cb9d06c97092b8
Nissya <kirito042004@gmail.com>
2024-03-06 16:39:57 +0100

    some spell modifiactions

--- lua/conf/Spell.lua
+++ lua/conf/Spell.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = "lua,python,c,java,json,js,r,rs,toggleterm,zsh,conf,sh,kts",
+	pattern = "lua,python,c,java,json,js,r,rs,toggleterm,zsh,conf,sh,kts,diff,yuck,css,scss,html",
 	callback = function()
 		vim.opt.spell = false
 	end
